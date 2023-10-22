80cc1970ac4a6a94ebac5cfc4463f11263c86f99
MireilleHossam <117849443+MireilleHossam@users.noreply.github.com>
2023-10-22 23:33:51 +0300

    Adding start and collision Sounds

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -6,15 +6,22 @@ public class PlayerCollision : MonoBehaviour
 {
 
     public PlayerMovement movement; // taking the script playerMovement (as a reference) and enabling it
-
+     void Start()
+    {
+       
+    }
     // collision built-in method
     public void OnCollisionEnter(Collision collisionInfo)
     {
         if(collisionInfo.collider.tag== "Obstacle")
         {
             Debug.Log("WE HIT SOMETHING!!");
-           // movement.enabled = false;
+            // movement.enabled = false;
+            collisionInfo.gameObject.GetComponent<SFXPlayer>().PlaySFX();
+
+                    
         }
+
     }
 
 }
