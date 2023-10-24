71dcc4a1edac65a1d3837513ac66ae99af5dc135
MireilleHossam <117849443+MireilleHossam@users.noreply.github.com>
2023-10-24 15:48:01 +0300

    LevelUp edition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,17 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false; // condition to stop the player from continuing movement after collision
-
     public float delayTheRestart = 1f;
+    public GameObject completeLevelUI;
+
+    // WINNING A GAME
+    public void CompleteLevel()
+    {
+        //Debug.Log("LEVEL WON!!");
+        completeLevelUI.SetActive(true); // will activaye the LevelCompletePanel that is disabled
+    }
+
+    //ENDING the game
     public void EndGame ()
     {
         if (gameHasEnded == false)
@@ -16,12 +25,12 @@ public class GameManager : MonoBehaviour
             gameHasEnded = true;
             Debug.Log("GAME OVER!!!!");
             Invoke("Restart", delayTheRestart); // delay the restart with a certain timing
-        }
-        
+        }    
     }
+    // RESTARTING THE GAME
     void Restart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // acessing the opened scene in unity
+         SceneManager.LoadScene(SceneManager.GetActiveScene().name); // acessing the opened scene in unity
 
     }
 
